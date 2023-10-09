11da21b7d47e521bc8cb40880871329109530286
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2023-10-09 23:57:38 +0100

    updated ProjectsBox, customButton has ability to either be a button or anchor. created footer component

--- src/components/customButton/CustomButton.tsx
+++ src/components/customButton/CustomButton.tsx
@@ -4,17 +4,31 @@ interface CustomButtonProps {
   label?: string;
   onClick?: () => void;
   className?: string;
+  href?: string;
 }
 
-const CustomButton = ({ label, onClick, className }: CustomButtonProps) => {
+const CustomButton = ({
+  label,
+  onClick,
+  className,
+  href,
+}: CustomButtonProps) => {
   const rootClassName = clsx(
-    "underline underline-offset-[10px] decoration-[var(--accent)] hover:text-[var(--accent)] h-12",
+    "underline underline-offset-[10px] decoration-[var(--accent)] hover:text-[var(--accent)] w-fit",
     className
   );
   return (
-    <button onClick={onClick} className={rootClassName}>
-      {label || "CONTACT ME"}
-    </button>
+    <>
+      {href ? (
+        <a className={rootClassName} href={href}>
+          {label}
+        </a>
+      ) : (
+        <button onClick={onClick} className={rootClassName}>
+          {label || "CONTACT ME"}
+        </button>
+      )}
+    </>
   );
 };
 
--- src/components/projectsBox/ProjectsBox.tsx
+++ src/components/projectsBox/ProjectsBox.tsx
@@ -9,23 +9,37 @@ const projects = [
     link: "",
     code: "",
   },
+  {
+    title: "MEMORY GAME",
+    tools: ["React", "TypeScript", "Tailwind"],
+    image: "",
+    link: "",
+    code: "",
+  },
+  {
+    title: "FRIDGE APP",
+    tools: ["React-Native", "TypeScript", "CSS", "Node.js", "MongoDB"],
+    image: "",
+    link: "",
+    code: "",
+  },
 ];
 
 const ProjectsBox = () => {
   return (
     <div className="w-full">
-      <div className="flex justify-between">
+      <div className="flex justify-between mb-10">
         <h1>Projects</h1>
         <CustomButton />
       </div>
       <ul>
         {projects.map((project, index) => {
           return (
-            <li key={index}>
+            <li className="mb-10" key={index}>
               <div>
                 {/* <Image src="" alt={project.title} /> */}
-                <h3>{project.title}</h3>
-                <ul className="grid grid-flow-col items-start ">
+                <h3 className="mb-2">{project.title}</h3>
+                <ul className="grid grid-cols-3 gap-4 whitespace-nowrap  items-start ">
                   {project.tools.map((tool, index) => {
                     return (
                       <li className=" w-fit" key={index}>
@@ -35,11 +49,13 @@ const ProjectsBox = () => {
                   })}
                 </ul>
               </div>
-              <div className="flex">
-                <CustomButton label="VIEW PROJECT" />
-                <CustomButton label="VIEW CODE" />
-                <a href={project.link}>Link</a>
-                <a href={project.code}>Code</a>
+              <div className="flex mt-6">
+                <CustomButton
+                  href="hello"
+                  className="mr-8"
+                  label="VIEW PROJECT"
+                />
+                <CustomButton href="hello" label="VIEW CODE" />
               </div>
             </li>
           );
