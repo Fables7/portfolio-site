bddfb392a3737db5ca62d5693b8bc54ba509f5cd
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2024-01-18 15:29:20 +0000

    Updated experience details

--- src/components/experienceBox/ExperienceBox.tsx
+++ src/components/experienceBox/ExperienceBox.tsx
@@ -1,14 +1,14 @@
 // const experienceItems = ["HTML", "CSS", "JavaScript", "Accessibility", "React"];
 const experienceItems = [
-  { skill: "HTML", years: 2 },
-  { skill: "CSS", years: 2 },
-  { skill: "HTML", years: 2 },
-  { skill: "JavaScript", years: 2 },
-  { skill: "TypeScript", years: 2 },
-  { skill: "React", years: 2 },
-  { skill: "React-Native", years: 2 },
+  { skill: "HTML", years: 3 },
+  { skill: "CSS", years: 3 },
+  { skill: "HTML", years: 3 },
+  { skill: "JavaScript", years: 3 },
+  { skill: "TypeScript", years: 3 },
+  { skill: "React", years: 3 },
+  { skill: "React-Native", years: 3 },
   { skill: "Node.js", years: 2 },
-  { skill: "MongoDB", years: 1 },
+  { skill: "MongoDB", years: 2 },
   { skill: "Solidity", years: 1 },
   { skill: "SQL", years: 1 },
 ];
